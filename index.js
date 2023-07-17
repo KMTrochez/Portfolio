892023d5926c5f62eaa098a660713cd4f5428ce7
KMTrochez <80430997+KMTrochez@users.noreply.github.com>
2023-07-16 23:00:11 -0700

    Update index.js

--- index.js
+++ index.js
@@ -5,21 +5,21 @@ const strings = [
     "software engineer",
     "front-end developer",
     "back-end developer",
+    "program analyst",
+    "data analyst",
+    "financial analyst",
+    "QA tester",
     "UI/UX designer",
   ];
-  alert("index.js is connected and executed!");
-  console.log("Changetext() function is being executed!");
-  
+
   // Function to change the text inside the <span> element
   function changeText() {
-    console.log("Changetext() function is being executed!");
-    alert("index.js is connected and executed!");
     const changingText = document.getElementById("changingText");
     let index = 0;
     setInterval(() => {
       changingText.textContent = strings[index];
       index = (index + 1) % strings.length;
-    }, 2000); // Change the text every 2 seconds (adjust as needed)
+    }, 1500); // Change the text every 1.5 seconds (adjust as needed)
   }
   
   // Call the function to start changing the text
